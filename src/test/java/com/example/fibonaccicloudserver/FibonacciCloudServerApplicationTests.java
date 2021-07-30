326b3d878b0344ecaeba26ccfc4d00f9712edcdf
Maxim Gorovets <gorovec3@gmail.com>
2021-07-30 10:04:18 +0300

    Test file

---
+++ src/test/java/com/example/fibonaccicloudserver/FibonacciCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fibonaccicloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciCloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
